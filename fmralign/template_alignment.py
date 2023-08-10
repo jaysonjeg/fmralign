90e5fa5e60bbe1b98521e5179b97202ce395cd94
Jayson Jeganathan <jayson.jeganathan@gmail.com>
2023-08-10 19:24:45 +1000

    add hyperalignment to template_alignment.py

--- fmralign/template_alignment.py
+++ fmralign/template_alignment.py
@@ -10,6 +10,7 @@ from joblib import Memory, Parallel, delayed
 from sklearn.base import BaseEstimator, TransformerMixin
 from fmralign.surf_pairwise_alignment import SurfacePairwiseAlignment
 
+'''
 def _rescaled_euclidean_mean(imgs, scale_average=False):
     """Make the Euclidian average of images
 
@@ -43,7 +44,6 @@ def _align_one_image_to_target(img,alignment_method,clustering,n_jobs,target,ali
     piecewise_estimator= SurfacePairwiseAlignment(alignment_method, clustering, n_jobs=n_jobs,alignment_kwargs=alignment_kwargs)
     piecewise_estimator.fit(img, target) 
     return piecewise_estimator
-
 def _align_images_to_template(
     imgs,
     template,
@@ -64,126 +64,32 @@ def _align_images_to_template(
     aligned_imgs = [piecewise_estimators[i].transform(imgs[i]) for i in range(len(imgs))] 
 
     return aligned_imgs, piecewise_estimators
+'''
 
-def _align_images_to_template_excluding_self(
-    imgs,
-    aligned_imgs,
-    alignment_method,
-    clustering,
-    n_bags,
-    memory,
-    memory_level,
-    n_jobs,
-    verbose,
-    alignment_kwargs,
-    scale_template=None
-):
-    """As above, but it will align each subject to a slightly different template which is the average of all subjects excluding that subject. This requires argument scale_template which will be passed to _rescaled_euclidean_mean
+
+def _align_images_to_target(source_imgs,target_img,clustering,alignment_method,alignment_kwargs):
     """
-
-    get_this_template = lambda i: _rescaled_euclidean_mean([item for index,item in enumerate(aligned_imgs) if index!=i], scale_template)
-    piecewise_estimators = Parallel(n_jobs=-1)(delayed(_align_one_image_to_target)(imgs[i],alignment_method,clustering,n_jobs,get_this_template(i),alignment_kwargs) for i in range(len(imgs)))       
-    aligned_imgs = [piecewise_estimators[i].transform(imgs[i]) for i in range(len(imgs))] 
-
-    return aligned_imgs, piecewise_estimators
-
-def _create_template(
-    imgs,
-    n_iter,
-    scale_template,
-    alignment_method,
-    clustering,
-    n_bags,
-    memory,
-    memory_level,
-    n_jobs,
-    verbose,
-    template_method=1,
-    include_current_subject=True,
-    alignment_kwargs={}
-):
-    """Create template through alternate minimization.  Compute iteratively :
-    * T minimizing sum(||R_i X_i-T||) which is the mean of aligned images (RX_i)
-    * align initial images to new template T
-        (find transform R_i minimizing ||R_i X_i-T|| for each img X_i)
-
-
-    Parameters
-    ----------
-    imgs: List of Niimg-like objects
-       See http://nilearn.github.io/manipulating_images/input_output.html
-       source data. Every img must have the same length (n_sample)
-    scale_template: boolean
-        If true, template is rescaled after each inference so that it keeps
-        the same norm as the average of training images.
-    n_iter: int
-       Number of iterations in the alternate minimization. Each image is
-       aligned n_iter times to the evolving template. If n_iter = 0,
-       the template is simply the mean of the input images.
-    template_method: int
-        1: same as original fmralign code
-        2: hyperalignment method, where published?, on first iteration the new template is the average of all previous aligned images
-        3: hyperalignment method in https://doi.org/10.1016/j.neuron.2011.08.026 and https://doi.org/10.1371/journal.pcbi.1006120, on first iteration the new template (for next subject) is the average of the previous template and the current subject's aligned image. Needs n_iter=1.  exclude_current_subject=True for https://doi.org/10.1371/journal.pcbi, or False for https://doi.org/10.1016/j.neuron.2011.08.026
-    include_current_subject: bool
-        whether to align subject's to the template including themself or not
-    All other arguments are the same are passed to PairwiseAlignment
-
-    Returns
-    -------
-    template: list of 3D Niimgs of length (n_sample)
-        Models the barycenter of input imgs
-    piecewise_estimators
+    Parallelize calling _align_one_image for all source_imgs
     """
-
-    aligned_imgs = imgs
-    for iter in range(n_iter):
-
-        if iter==0 and template_method != 1: 
-            assert(n_iter==2) #hyperalignment method needs 2 iterations after the first round
-            aligned_imgs=[imgs[0]] 
-            current_template = imgs[0]
-            for i in range(1,len(imgs)):
-                piecewise_estimator= SurfacePairwiseAlignment(alignment_method, clustering, n_jobs=n_jobs,alignment_kwargs=alignment_kwargs)
-                piecewise_estimator.fit(imgs[i], current_template)
-                new_img = piecewise_estimator.transform(imgs[i])
-                aligned_imgs.append(new_img)
-                if template_method==2: #new template is average of all previous aligned images
-                    current_template = _rescaled_euclidean_mean(aligned_imgs,scale_template)
-                elif template_method==3: #new template is average of previous template and latest image
-                    current_template = _rescaled_euclidean_mean([current_template, new_img],scale_template)  
-
-        if include_current_subject==True:
-            template = _rescaled_euclidean_mean(aligned_imgs, scale_template)
-            aligned_imgs, piecewise_estimators = _align_images_to_template(
-                imgs,
-                template,
-                alignment_method,
-                clustering,
-                n_bags,
-                memory,
-                memory_level,
-                n_jobs,
-                verbose,
-                alignment_kwargs
-            )
-        elif include_current_subject==False: 
-            aligned_imgs, piecewise_estimators = _align_images_to_template_excluding_self(
-                imgs,
-                aligned_imgs,
-                alignment_method,
-                clustering,
-                n_bags,
-                memory,
-                memory_level,
-                n_jobs,
-                verbose,
-                alignment_kwargs, scale_template=None)
-            template = _rescaled_euclidean_mean(aligned_imgs, scale_template)
-
-    return template, piecewise_estimators
-
-
-
+    aligners= Parallel(n_jobs=-1,prefer='processes')(delayed(_align_one_image)(source_img,target_img,clustering,alignment_method,alignment_kwargs,n_jobs=1,parallel_type='threads') for source_img in source_imgs)
+    return aligners
+def _align_one_image(source_img,target_img,clustering,alignment_method,alignment_kwargs,n_jobs=1,parallel_type='threads'):
+    aligner= SurfacePairwiseAlignment(alignment_method, clustering, n_jobs=n_jobs, parallel_type=parallel_type,alignment_kwargs=alignment_kwargs)
+    aligner.fit(source_img, target_img)
+    return aligner
+def _align_one_image_without_self(img,template,level_1_aligned_img,n_imgs,normalizer_template,clustering,alignment_method,alignment_kwargs,n_jobs=1,parallel_type='threads'):
+    #similar to _align_one_image, except it also subtracts the subject's aligned image from the template before aligning the other images to it
+    template = (template * n_imgs - level_1_aligned_img) / (n_imgs - 1)
+    template = normalizer_template(template)
+    aligner = _align_one_image(img,template,clustering,alignment_method,alignment_kwargs,n_jobs=n_jobs,parallel_type=parallel_type)
+    return aligner
+def zscore(X):
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    return scaler.fit_transform(X)
+def normalize_to_unit_norm(X):
+    return X/np.linalg.norm(X)
+   
 
 class TemplateAlignment(BaseEstimator, TransformerMixin):
     """
@@ -192,22 +98,7 @@ class TemplateAlignment(BaseEstimator, TransformerMixin):
     new contrast for target subject.
     """
 
-    def __init__(
-        self,
-        alignment_method="identity",
-        clustering=None,
-        scale_template=False,
-        n_iter=2,
-        save_template=None,
-        n_bags=1,
-        memory=Memory(location=None),
-        memory_level=0,
-        n_jobs=1,
-        verbose=0,
-        template_method=1,
-        include_current_subject=True,
-        alignment_kwargs={}
-    ):
+    def __init__(self,alignment_method="identity",clustering=None,alignment_kwargs={}):
         """
         Parameters
         ----------
@@ -217,105 +108,83 @@ class TemplateAlignment(BaseEstimator, TransformerMixin):
             'ridge_cv', 'permutation', 'diagonal'
             * or an instance of one of alignment classes (imported from
             functional_alignment.alignment_methods)
-        n_pieces: int, optional (default = 1)
-            Number of regions in which the data is parcellated for alignment.
-            If 1 the alignment is done on full scale data.
-            If > 1, the voxels are clustered and alignment is performed on each
-            cluster applied to X and Y.
-        clustering : array (int,)
-            Clustering labels for each voxel
-        scale_template: boolean, default False
-            rescale template after each inference so that it keeps
-            the same norm as the average of training images.
-        n_iter: int
-           number of iteration in the alternate minimization. Each img is
-           aligned n_iter times to the evolving template. If n_iter = 0,
-           the template is simply the mean of the input images.
-        save_template: None or string(optional)
-            If not None, path to which the template will be saved.
-        n_bags: int, optional (default = 1)
-            If 1 : one estimator is fitted.
-            If >1 number of bagged parcellations and estimators used.
-        memory: instance of joblib.Memory or string (default = None)
-            Used to cache the masking process and results of algorithms.
-            By default, no caching is done. If a string is given, it is the
-            path to the caching directory.
-        memory_level: integer, optional (default = None)
-            Rough estimator of the amount of memory used by caching.
-            Higher value means more memory for caching.
-        n_jobs: integer, optional (default = 1)
-            The number of CPUs to use to do the computation. -1 means
-            'all CPUs', -2 'all CPUs but one', and so on.
-        verbose: integer, optional (default = 0)
-            Indicate the level of verbosity. By default, nothing is printed.
-        template_method: int
-            1: same as original fmralign code
-            2: hyperalignment method, on first iteration the new template is the average of all previous aligned images
-            3: hyperalignment method, on first iteration the new template is the average of the previous template and the latest image
+        clustering: array: shape (nvertices,), dtype int
+            Clustering labels for each vertex
         alignment_kwargs: dict
             Additional keyword arguments to pass to the alignment method
         """
         self.template = None
-        self.template_history = None
         self.alignment_method = alignment_method
         self.clustering = clustering
-        self.n_iter = n_iter
-        self.scale_template = scale_template
-        self.save_template = save_template
-        self.n_bags = n_bags
-        self.memory = memory
-        self.memory_level = memory_level
-        self.n_jobs = n_jobs
-        self.verbose = verbose
-        self.template_method=template_method
-        self.include_current_subject=include_current_subject
         self.alignment_kwargs = alignment_kwargs
+
+    def make_template(self,imgs,n_iter,do_level_1,level1_equal_weight,normalize_imgs,normalize_template,remove_self):
+        """
+        Make template image from a list of images. Combines elements of code from fmralign package and pyMVPA2 package, in particular using some similar naming as pyMVPA2. This function does 'level 1' (optional) and 'level 2' of pyMVPA2. Level 1 involves iteratively aligning images to an evolving template. Level 2 simultaneously aligns all images to a single template
+        For standard hyperalignment: do_level_1=True, normalize_imgs='zscore', normalize_template='zscore', remove_self=True, level1_equal_weight=False
+        For standard fmralign: do_level_1=False, normalize_imgs=None, normalize_template=None,remove_self=False
+        Parameters
+        ----------
+        imgs: list(nsubjects) of arrays (nsamples,nvertices)
+            Source data
+        n_iter: int
+            number of iterations at level 2. Set to zero just to use mean of level 1 images as the template
+        do_level_1: bool
+            whether to do level 1. 
+        level1_equal_weight: bool
+            in level 1, weight each subject equally in the template. 
+        normalize_imgs: 'zscore', 'rescale', or None
+            To normalize each aligned image before taking the mean to make template. 'rescale' scales each image to have unit matrix Frobenius norm. 'zscore' zscores each column, so that each image contributes near-equally to each vertex. For hyperalignment, pick 'zscore'. 'rescale' might have problems if the original matrix norm is too big for dtype e.g. float16
+        normalize_template: 'zscore', 'rescale', or None
+            To normalize the template at each iteration. 
+        remove_self: bool
+            in level 2, subtract that subject's aligned image from the template before aligning the other images to it, so that the subject is not included in the template that they are aligned to. 
+        """
+
+
+        normalize_dict = {'zscore':zscore, 'rescale':normalize_to_unit_norm, None:lambda X: X}
+        normalizer_imgs = normalize_dict[normalize_imgs]
+        normalizer_template = normalize_dict[normalize_template]
+        if (normalize_template=='rescale' or normalize_imgs=='rescale') and (imgs[0].dtype==np.float16):
+            imgs = [i.astype(np.float32) for i in imgs] #rescaling might have problems if the original matrix norm is too big for dtype e.g. float16
+
+        #Level 1
+        if do_level_1: 
+            template = normalizer_template(imgs[0]) #initial template is first subject's image
+            aligned_imgs = [imgs[0]] #consider renaming this variable as aligned_imgs !!!
+            for i in range(1,len(imgs)):
+                aligner = _align_one_image(imgs[i],template,self.clustering,self.alignment_method,self.alignment_kwargs,n_jobs=-1,parallel_type='threads')
+                new_img = aligner.transform(imgs[i]) #image aligned to template
+                new_img = normalizer_imgs(new_img)
+                aligned_imgs.append(new_img) #slow step, maybe initialise as empty numpy array of objects? !!!
+                if level1_equal_weight: 
+                    template = np.average([template, new_img], weights=(i, 1.0), axis=0) #think they have it backwards in pymvpa
+                else: 
+                    template = np.mean([template,new_img],axis=0) #in this scenario, earlier images have lesser contribution to the template
+                template = normalizer_template(template)
+        else:
+            aligned_imgs = [normalizer_imgs(i) for i in imgs]
+        
+        #Level 2
+        template = np.mean(aligned_imgs,axis=0) #initial template is average of aligned images
+        for iter in range(n_iter):
+            if remove_self: 
+                n_imgs = len(imgs)
+                aligners = Parallel(n_jobs=-1,prefer='processes')(delayed(_align_one_image_without_self)(img,template,aligned_img,n_imgs,normalizer_template,self.clustering,self.alignment_method,self.alignment_kwargs,n_jobs=1,parallel_type='threads') for img,aligned_img in zip(imgs,aligned_imgs))    
+            else:      
+                template = normalizer_template(template)                
+                aligners = _align_images_to_target(imgs,template,self.clustering,self.alignment_method,self.alignment_kwargs)
+            aligned_imgs = [aligners[i].transform(imgs[i]) for i in range(len(imgs))] 
+            aligned_imgs = [normalizer_imgs(i) for i in aligned_imgs]
+            template = np.mean(aligned_imgs,axis=0)
+        self.template = normalizer_template(template)
+
 
     def fit_to_template(self,imgs):
         """
         Fit new imgs to pre-calculated template
         """
-        _,self.estimators = _align_images_to_template(imgs, self.template, self.alignment_method, self.clustering, self.n_bags, self.memory, self.memory_level,self.n_jobs, self.verbose,self.alignment_kwargs)
-    def fit(self, imgs):
-        """
-        Learn a template from source images, using alignment.
-
-        Parameters
-        ----------
-        imgs: List of 4D Niimg-like or List of lists of 3D Niimg-like
-            Source subjects data. Each element of the parent list is one subject
-            data, and all must have the same length (n_samples).
-
-        Returns
-        -------
-        self
-
-        Attributes
-        ----------
-        self.template: 4D Niimg object
-            Length : n_samples
-
-        """
-        # Assume imgs is a list (nsubjects) of arrays(nsamples,nvertices)
-        assert(imgs[0].shape[1]==self.clustering.shape[0])
-        self.template, self.estimators = _create_template(
-            imgs,
-            self.n_iter,
-            self.scale_template,
-            self.alignment_method,
-            self.clustering,
-            self.n_bags,
-            self.memory,
-            self.memory_level,
-            self.n_jobs,
-            self.verbose,
-            self.template_method,
-            self.include_current_subject,
-            self.alignment_kwargs,
-        )
-        if self.save_template is not None:
-            self.template.to_filename(self.save_template)
-
+        self.estimators = _align_images_to_target(imgs,self.template,self.clustering,self.alignment_method,self.alignment_kwargs) 
 
     def transform(self,X,index):
         #Transform data array X with subject 'index''s aligner to the template 
